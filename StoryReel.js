315f8b97a324e8fa51935860493e9dbe937d1f22
Sai-Marala <117969980+Sai-Marala@users.noreply.github.com>
2022-11-29 10:48:37 -0800

    Add files via upload

---
+++ StoryReel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './StoryReel.css';
+import Story from './Story';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+                image="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/p960x960/119520615_153896206410888_5247635892986548095_o.jpg?_nc_cat=106&_nc_sid=110474&_nc_ohc=CJmsF3rvQf4AX_6a51Z&_nc_ht=scontent.fbwa3-1.fna&tp=6&oh=05859eae03caa6b2091a1dd9ffc9632c&oe=5F8F4F54"
+                profileSrc="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/109538782_101348904998952_757268889534115672_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=foCCJLY4GjQAX-kII_m&_nc_ht=scontent.fbwa3-1.fna&oh=4eb9465dcbf848e53babc0a8a0b7b905&oe=5F913A33"
+                title="Quotes and Status"
+            />
+            <Story
+                image="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/p720x720/90333828_107930217513177_360077040050765824_o.jpg?_nc_cat=101&_nc_sid=7aed08&_nc_ohc=HFiW_DViQisAX89olMe&_nc_ht=scontent.fbwa3-1.fna&tp=6&oh=8bcf349e3abe72561ff97e71f7eca653&oe=5F8EBF64"
+                profileSrc="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/91375566_117376876568511_4012324311523131392_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=lUjSty6pSLcAX8XI3qP&_nc_ht=scontent.fbwa3-1.fna&oh=6310ac17bd4cb219d5ec38c30c566398&oe=5F91BC43"
+                title="SAI krupa"
+            />
+            <Story
+                image="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/p960x960/105325612_141478867550570_2779684418003956702_o.jpg?_nc_cat=108&_nc_sid=8024bb&_nc_ohc=bDmFg6K3kHkAX_clcin&_nc_ht=scontent.fbwa3-1.fna&tp=6&oh=a2da7f12f27d4d3d3808a6728e6b6f73&oe=5F8EB8DC"
+                profileSrc="https://scontent.fbwa3-1.fna.fbcdn.net/v/t31.0-8/p720x720/27748091_731667903685773_3806696550266953128_o.jpg?_nc_cat=109&_nc_sid=7aed08&_nc_ohc=IU2_BYC-VooAX_bHkOF&_nc_ht=scontent.fbwa3-1.fna&tp=6&oh=07176f88f72f1109f96672ea109962ef&oe=5F8ECAAB"
+                title="SIRI"
+            />
+            <Story
+                image="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/101947596_295943648457384_6925415545215654904_n.jpg?_nc_cat=110&_nc_sid=7aed08&_nc_ohc=zGOhDU3roN4AX9Dtkgd&_nc_ht=scontent.fbwa3-1.fna&oh=9a0e2265b9cb596325f240eb20bbdb80&oe=5F8F4734"
+                profileSrc="https://scontent.fbwa3-1.fna.fbcdn.net/v/t1.0-9/117240821_342020823849666_8927678427820593151_n.jpg?_nc_cat=108&_nc_sid=7aed08&_nc_ohc=8xAH9ZN7DgoAX8FsXz3&_nc_ht=scontent.fbwa3-1.fna&oh=f4e8c4c61e30a27419dbae6f9e16ad90&oe=5F90C20D"
+                title="SONI"
+            />
+            
+        </div>
+    )
+}
+
+export default StoryReel;
+
